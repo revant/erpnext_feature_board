c5e496aa928c67f87315f49d3c94c8824e7abc57
Rohan Bansal <rohan@parsimony.com>
2021-07-04 13:51:20 +0530

    fix: cleanup review request logic

--- erpnext_feature_board/erpnext_feature_board/doctype/review_request/review_request.py
+++ erpnext_feature_board/erpnext_feature_board/doctype/review_request/review_request.py
@@ -15,16 +15,20 @@ class ReviewRequest(Document):
 		self.name = str(uuid.uuid4())
 
 	def validate(self):
+		self.validate_user()
+		self.validate_status()
+
+	def validate_user(self):
 		if (
-			frappe.session.user == "Guest"
-			or not frappe.get_conf().get("developer_mode")
-			and frappe.session.user == "Administrator"
+			frappe.session.user in ["Guest", "Administrator"]
+			and not frappe.get_conf().get("developer_mode")
 		):
-			frappe.throw(_("Invalid User, Guest or Administrator not allowed"))
+			frappe.throw(_("Invalid User: Guest or Administrator not allowed"))
 
 		if not self.user:
 			self.user = frappe.session.user
 
+	def validate_status(self):
 		if not self.request_status:
 			self.request_status = "Open"
 
@@ -43,7 +47,7 @@ def get_test_user_password(review_request):
 
 @frappe.whitelist(methods=["POST"])
 def create_test_user_for_improvement(improvement_name, review_request_uuid):
-	s = requests.Session()
+	session = requests.Session()
 
 	if "System Manager" not in frappe.get_roles(frappe.session.user):
 		frappe.throw(_("Insufficient Permission"))
@@ -52,28 +56,21 @@ def create_test_user_for_improvement(improvement_name, review_request_uuid):
 	review_request = frappe.get_doc("Review Request", review_request_uuid)
 
 	if review_request.request_type != "Add Testing User":
-		frappe.throw(
-			_("Incorrect Request Type : {0}".format(review_request.request_type))
-		)
-
+		frappe.throw(_(f"Incorrect request type: {review_request.request_type}"))
 	if review_request.request_status != "Open":
-		frappe.throw(
-			_("Incorrect Request Status : {0}".format(review_request.request_status))
-		)
-
+		frappe.throw(_(f"Incorrect request status: {review_request.request_status}"))
 	if not improvement.site_url:
-		frappe.throw(_("Site URL Not found for {0}".format(improvement.name)))
-
+		frappe.throw(_(f"Site URL not found for {improvement.name}"))
 	if improvement.deployment_status != "Ready":
-		frappe.throw(_("Deployment Status Not Ready for {0}".format(improvement.name)))
+		frappe.throw(_(f"Deployment is not ready for {improvement.name}"))
 
-	s.post(
+	session.post(
 		f"{improvement.site_url}/api/method/login",
 		data={
 			"usr": "Administrator",
 			"pwd": improvement.get_password("site_admin_password"),
 		},
-	).json()
+	)
 
 	email = frappe.mock("email")
 	first_name = frappe.mock("first_name")
@@ -86,7 +83,7 @@ def create_test_user_for_improvement(improvement_name, review_request_uuid):
 		"roles": [{"role": "System Manager"}],
 	}
 
-	s.post(f"{improvement.site_url}/api/resource/User", data=json.dumps(user)).json()
+	session.post(f"{improvement.site_url}/api/resource/User", data=json.dumps(user))
 
 	review_request.test_user_name = email
 	review_request.test_user_password = new_password
